61f59cc003ade17ca9b139421e4bde6297175f3c
steph-en-m <mstevejean@gmail.com>
2020-11-02 13:30:17 +0300

    add validation to Keywords table

--- app/models.py
+++ app/models.py
@@ -5,12 +5,14 @@ class Users(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    email = db.Column(db.String())
+    email = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.Integer)
     role = db.Column(db.String())
 
-    def __init__(self, name, role):
+    def __init__(self, name, email, password, role):
         self.name = name
+        self.email = email
+        self.password = password
         self.role = role
 
     def __repr__(self):
@@ -20,13 +22,14 @@ class Users(db.Model):
         return {
             "id": self.id,
             "name": self.name,
+            "email": self.email,
             "role": self.role
         }
 
 class Keywords(db.Model):
     __tablename__="keywords"
     id  = db.Column(db.Integer, primary_key=True)
-    keyword = db.Column(db.String())
+    keyword = db.Column(db.String(), unique=True, nullable=False)
 
     def __init__(self, keyword):
         self.keyword = keyword
@@ -34,10 +37,15 @@ class Keywords(db.Model):
     def __repr__(self):
         return f"<id {self.id}>"
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "Keyword": self.keyword
+    def serialize(self, _id=True):
+        if _id:    
+            return {
+                "id": self.id,
+                "Keyword": self.keyword
+                }
+        else:
+            return {
+                "Keyword": self.keyword
             }
 
 class Tweets(db.Model):
