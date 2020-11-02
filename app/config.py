1c080a41947f807785331f36d34839695535ffed
steph-en-m <mstevejean@gmail.com>
2020-11-02 13:27:35 +0300

    add tweepy config credentials

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+# Tweepy Configurations
+CONSUMER_KEY = "25O70MpmGuKOVn2z4q6Pnqg0g"
+CONSUMER_SECRET = "RhsCbkW1IvqWywqXGeNl7zOzcfCFGBEnpx07Csk9lHLe0OXtdw"
+ACCESS_TOKEN = "940556535222231040-YwDy453At4zZkZBKD1no1wLOSFbnXW4"
+ACCESS_TOKEN_SECRET = "Z7vuvj4jauPJuy51MdHw8lc3HtGLh2df5bCsubjCQDsxB"
+
 
 class Config:
     DEBUG = False
